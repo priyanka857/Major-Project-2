b6ff20e33374679a4100bff2ef52c3e7e434d38d
PriyankaPandiyan <priyankasuresh857@gmail.com>
2025-08-28 22:37:42 +0530

    Fix chat

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ const { Server } = require("socket.io");
 const connectDB = require("./config/db");
 const chatRoutes = require("./routes/chatRoutes");
 const postRoutes = require("./routes/postRoutes");
-const Chat = require("./models/chat"); // 🔥 Fix: import Chat model
-
+const Chat = require("./models/Chat");
 dotenv.config();
 const app = express();
 
